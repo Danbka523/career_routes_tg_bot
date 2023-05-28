1d13f7742965bbeafcfea380489ff14e9f48cf49
Daniil Bashinskiy <gordonanet12@gmail.com>
2023-05-28 15:26:00 +0300

    modified:   bot.py

--- bot.py
+++ bot.py
@@ -32,34 +32,67 @@ def create_key(text,callback_data):
 def get_text_messages(message,data=""):
     if message.text=='/start':
         bot.send_photo(chat_id=message.chat.id, photo=open('assets/img.jpg','rb'))
-        text= "Привет! Меня зовут МаКар! Я буду рад помочь тебе с поиском — просто нажми кнопку, и я расскажу всё, что знаю!"
+        text= "Привет! Меня зовут Макар! Я буду рад помочь тебе с поиском — просто нажми кнопку, и я расскажу всё, что знаю!"
         keyboard = types.InlineKeyboardMarkup()
-        key_yes = create_key(text="Начать поиск!", callback_data="spec")
-        key_about = create_key(text="О проекте", callback_data="about")
+        key_yes = create_key(text="Начать поиск!", callback_data="br")
+        key_about = create_key(text="Сведения", callback_data="about")
         key_help = create_key(text="Помощь", callback_data="help")
-        keyboard.add(key_help)
         keyboard.add(key_yes)
+        #keyboard.add(key_help)
         keyboard.add(key_about)
         bot.send_message(message.from_user.id,text=text, reply_markup=keyboard)
 
     if data=="main_menu":
-        text= "Привет! Меня зовут МаКар! Я буду рад помочь тебе с поиском — просто нажми кнопку, и я расскажу всё, что знаю!"
+        text= "Привет! Меня зовут Макар! Я буду рад помочь тебе с поиском — просто нажми кнопку, и я расскажу всё, что знаю!"
         keyboard = types.InlineKeyboardMarkup()
-        key_yes = create_key(text="Начать поиск!", callback_data="spec")
-        key_about = create_key(text="О проекте", callback_data="about")
+        key_yes = create_key(text="Начать поиск!", callback_data="br")
+        key_about = create_key(text="Сведения", callback_data="about")
         key_help = create_key(text="Помощь", callback_data="help")
-        keyboard.add(key_help)
         keyboard.add(key_yes)
+        #keyboard.add(key_help)
         keyboard.add(key_about)
         bot.edit_message_text(chat_id=message.chat.id,message_id=message.message_id,  text=text, reply_markup=keyboard)
 
-
-def ask_spec(message,i):
-    text="Выбери специальность"
+def ask_branch(message, i):
+    text="Выбери отрасль"
     keyboard = types.InlineKeyboardMarkup()
 
     query = '''
-    select job_name from jobs
+    select branch_name from branches
+    '''
+
+    connection = sqlite3.connect("maindb.sqlite3")
+    branches=execute_read_query(connection,query)
+
+    for t in range(i, i+2):
+        if t<len(branches):
+            key=create_key(branches[t][0],callback_data="spec;0"+branches[t][0])
+            keyboard.add(key)
+
+    if i+2<len(branches):
+        key=create_key("Еще?", callback_data=f"br;{i+2}")
+        keyboard.add(key)
+
+    if i!=0:
+        key=create_key("Посмотреть прошлые?", callback_data=f"br;{i-2}")
+        keyboard.add(key)
+    
+   
+
+    main_menu_key = create_key("На главное меню", "main_menu")
+    keyboard.add(main_menu_key)
+    bot.edit_message_text(chat_id=message.chat.id,message_id=message.message_id, text=text, reply_markup=keyboard)
+
+
+def ask_spec(message,data,i):
+    text="Выбери специальность"
+    keyboard = types.InlineKeyboardMarkup()
+
+    query = f'''
+    select job_name from jobs as j
+    join branches as b
+    on j.branch_id = b.branch_id
+    where b.branch_name = "{data}"
     '''
 
     connection = sqlite3.connect("maindb.sqlite3")
@@ -67,17 +100,18 @@ def ask_spec(message,i):
 
     for t in range(i, i+2):
         if t<len(jobs):
-            key=create_key(jobs[t][0],callback_data="job;"+jobs[t][0])
+            key=create_key(jobs[t][0],callback_data="job;"+jobs[t][0]+"*"+data)
             keyboard.add(key)
 
     if i+2<len(jobs):
-        key=create_key("Еще?", callback_data=f"spec;{i+2}")
+        key=create_key("Еще?", callback_data=f"spec;{i+2}{data}")
         keyboard.add(key)
 
     if i!=0:
-        key=create_key("Посмотреть прошлые?", callback_data=f"spec;{i-2}")
+        key=create_key("Посмотреть прошлые?", callback_data=f"spec;{i-2}{data}")
         keyboard.add(key)
-
+    key_br = create_key(text="Выбрать снова отрасль", callback_data=f"br")
+    keyboard.add(key_br)
     main_menu_key = create_key("На главное меню", "main_menu")
     keyboard.add(main_menu_key)
     bot.edit_message_text(chat_id=message.chat.id,message_id=message.message_id, text=text, reply_markup=keyboard)
@@ -85,6 +119,7 @@ def ask_spec(message,i):
 
 def ask_type_event(message,data):
     print(data)
+    dt=data.split("*")
     text = "Выбери что тебя интересует"
     keyboard = types.InlineKeyboardMarkup()
 
@@ -95,9 +130,9 @@ def ask_type_event(message,data):
     events = execute_read_query(connection,query)
 
     for event in events:
-        key=create_key(event[0],callback_data="event;0"+data+"*"+event[0])
+        key=create_key(event[0],callback_data="event;0"+event[0]+"*"+data)
         keyboard.add(key)
-    key_yes = create_key(text="Выбрать снова специальность", callback_data="spec")
+    key_yes = create_key(text="Выбрать снова специальность", callback_data=f"spec;0{dt[1]}")
     keyboard.add(key_yes)
     bot.edit_message_text(chat_id=message.chat.id,message_id=message.message_id, text=text, reply_markup=keyboard)
 
@@ -105,15 +140,15 @@ def display_events(message,data, i):
     dt=data.split("*")
     
     print(data)
-    keyboard = types.InlineKeyboardMarkup()
-    text = f"Список:{dt[1]}"   
+    keyboard = types.InlineKeyboardMarkup(row_width=3)
+    text = f"Список:{dt[0]}"   
     query_t = f'''
         SELECT event_name FROM job_events as je
         join events as e
         on je.event_type_id = e.event_id
         join jobs as j
         on je.event_job_id = j.job_id
-        where j.job_name="{dt[0]}" and e.event_type="{dt[1]}"
+        where j.job_name="{dt[1]}" and e.event_type="{dt[0]}"
         
         '''
     connection = sqlite3.connect("maindb.sqlite3")
@@ -128,40 +163,45 @@ def display_events(message,data, i):
     if i+2<len(events):
         key=create_key("Еще?", callback_data=f"event;{i+2}{data}")
         keyboard.add(key)
+        
     if i!=0:
         key=create_key("Посмотреть прошлые?", callback_data=f"event;{i-2}{data}")
         keyboard.add(key)
     
-    key_type = create_key(text="Выбрать снова тип", callback_data=f"job;{dt[0]}")
+    key_type = create_key(text="Выбрать снова тип", callback_data=f"job;{dt[1]}*{dt[2]}")
     keyboard.add(key_type)
     bot.edit_message_text(chat_id=message.chat.id,message_id=message.message_id, text=text, reply_markup=keyboard)
 
 def display_event(message,data):
     dt=data.split("*")
-    event=data.split("*")[2]
+    event=data.split("*")[3]
     keyboard = types.InlineKeyboardMarkup()
 
     query = f'''
-    select event_name, event_desc from job_events
+    select event_name, event_desc, date, source from job_events
     where event_name="{event}"
     '''
 
     connection = sqlite3.connect("maindb.sqlite3")
     info=execute_read_query(connection,query)
-    text=f"{info[0][0]}\n{info[0][1]}"
+
+    text=f"{info[0][0]}\n{info[0][1]}\nКогда:{info[0][2]}\nПодробнее:{info[0][3]}"
+
     key_back = create_key(text="Назад", callback_data=f"event;0{dt[0]}*{dt[1]}")
     keyboard.add(key_back)
+
     main_menu_key = create_key("На главное меню", "main_menu")
     keyboard.add(main_menu_key)
+
     bot.edit_message_text(chat_id=message.chat.id,message_id=message.message_id, text=text,reply_markup=keyboard )
 
 def display_about(message):
-    text =  "\"Маршруты карьеры\" разработали для вас помощника в вопросах трудоустройства и практики! МаКар с удовольствием поделится самыми актуальными новостями, собранными со всего города! Больше никаких многочасовых поисков — только пара кликов, только свежие сведенья."
-    social = "Наша группа в ВК:vk.com/danbka523"
+    text =  "\"Маршруты карьеры\" разработали для вас помощника в вопросах трудоустройства и практики\! Макар с удовольствием поделится самыми актуальными новостями, собранными со всего города\! Больше никаких многочасовых поисков — только пара кликов, только свежие сведенья\."
+    social = "[Наша группа в ВК](vk.com/public220791588)"
     keyboard = types.InlineKeyboardMarkup()
-    key_back = create_key("Вернуться", "main_menu")
+    key_back = create_key("На главное меню", "main_menu")
     keyboard.add(key_back)
-    bot.edit_message_text(chat_id=message.chat.id,message_id=message.message_id, text=f"{text}\n{social}",reply_markup=keyboard )
+    bot.edit_message_text(chat_id=message.chat.id,message_id=message.message_id, text=f"{social}\n{text}",reply_markup=keyboard, parse_mode='MarkDownV2' )
 
 def display_help(message):
     text="В этом разделе будут предложены консулттации с экспертами(?????)\nРазличные полезные статьи и тесты тут тоже будут присутствовать"
@@ -172,11 +212,16 @@ def display_help(message):
 
 @bot.callback_query_handler(func=lambda call: True)
 def callback_worker(call):
-    if call.data[:4] == "spec":
+
+    if call.data[:2]=="br":
         i=0
-        if len(call.data)>4:
-            i=int(call.data[5:6])
-        ask_spec(call.message,i)
+        if (len(call.data)>3):
+            i = int(call.data[4:5])
+        ask_branch(call.message,i)
+
+    if call.data[:4] == "spec":
+        i=int(call.data[5:6])
+        ask_spec(call.message,call.data[6:],i)
         
     if call.data[:3]=="job":
         ask_type_event(call.message,call.data[4:])
